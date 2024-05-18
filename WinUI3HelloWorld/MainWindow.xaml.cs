3ef313a844beff5935682f896c7a09dc149e435d
Livan <tojkuv@outlook.com>
2024-05-18 00:27:50 -0500

    Completed the WinUI3 example

---
+++ WinUI3HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI3HelloWorld
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            var welcomeDialog = new ContentDialog()
+            {
+                Title = "Hello from HelloWorld",
+                Content = "Welcome to your first Windows App SDK app.",
+                CloseButtonText = "Ok",
+                XamlRoot = myButton.XamlRoot,
+            };
+            await welcomeDialog.ShowAsync();
+        }
+    }
+}
